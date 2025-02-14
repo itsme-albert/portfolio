89bbc45c42dd91e487d6d18e246ba1e304fda87f
albert <albertmejicoredila@gmail.com>
2025-02-14 15:10:24 +0800

    ok

--- src/app/components/navigations/Navbar.tsx
+++ src/app/components/navigations/Navbar.tsx
@@ -36,7 +36,7 @@ function Navbar() {
               {navItems.map((item, index) => (
                 <li key={index} className='flex items-center'>
                   {item.icon}
-                  <a href={item.href} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                  <a href={item.href} className="block py-2 text-white px-3 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                     {item.name}
                   </a>
                 </li>
